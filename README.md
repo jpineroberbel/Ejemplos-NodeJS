22b692d9a61650a9fb7fb9253d5e390f8062f0be
Jose Piñero <jpineroberbel@gmail.com>
2017-02-15 12:00:27 +0100

    Ejemplos para Ejercicios #1

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # Ejemplos de Uso de NodeJS 
-d
+
+Relación de ejemplos usados en clase para explicar NodeJS + Express
+
+## Ejercicios #1
+1 Probar cómo capturar post, put, delete
+2 Probar a capturar el paso de parámetros al server
+- En el archivo de rutas (routes/index.js) se puede encontrar como tratar cada ejemplos
+3 Probar a trabajar con expresiones regulares en las rutas
+En el archivo de rutas (routes/index.js) se puede encontrar como tratar cada ejemplos
+
+
+
+
+Jose A. Piñero Berbel
+
+Desarrollo Web en Aplicaciones Cliente
+IES Francisco Ayala
--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,21 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+// Ejemplo ruta con parámetros
+router.get('/param/:param1', function (req, res) {
+  res.send("He recibido como parámetro el valor: "+req.params["param1"])
+})
+
+// Ejemplo get con expresiones regulares
+router.get('/expreg/*/',function (req, res) {
+  res.send('Entra en caso con expresion regular, siendo la subcarpeta: '+req.params[0])
+})
+
+// Otro ejemplo get con expresiones regulares
+router.get('/*/index.html',function (req, res) {
+  res.send('Este otro ejemplo de expre. regular, busca una carpeta <strong>'+req.params[0]+'</strong> y dentro de ella buscaría index.html')
+})
+
+// Ejemplos de cómo capturar post, put y delete
+//router.post('/ejemploPost',function()
 module.exports = router;
